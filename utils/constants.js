9cc7199b84c123b1d7550ac513caa07c574f9eb8
viver77 <ya.ya-vs@yandex.ru>
2021-08-14 05:47:43 +0300

    14.08

---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+// Массив доменов, с которых разрешены кросс-доменные запросы
+exports.allowedCors = [
+  'https://diploma-1.nomoredomains.club',
+  'http://diploma-1.nomoredomains.club',
+  'https://api.diploma-1.nomoredomains.monster',
+  'http://api.diploma-1.nomoredomains.monster',
+  'localhost:3000',
+];
